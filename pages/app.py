f10aafd2a66a7324cf87e018bbbb34c9862318b6
kreamsoup-SH <alexalex225225@gmail.com>
2024-02-17 12:14:41 +0900

    applied cfg

--- pages/app.py
+++ pages/app.py
@@ -7,21 +7,19 @@ from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 import os
+import json
 
-def main():
+def main(cfg):
     # 애플리케이션 초기화 및 페이지 설정
     st.set_page_config(page_title="Welfare King", layout="wide")
     # 페이지 상태 관리
     if 'page' not in st.session_state:
         st.session_state.page = 'loading'
 
-
     # 로딩 화면
     if st.session_state.page == 'loading':
         st.title("필요한 정보를 불러오는중")
         st.write("잠시 기다려주세요...")
-        # OpenAI API 키 설정
-        os.environ['OPENAI_API_KEY'] = "<APIKEY HERE>"
         embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
         st.write("임베딩 호출 완료")
         print('임베딩 호출 완료')
@@ -74,4 +72,11 @@ def main():
         chatengine.display_chatbot()
 
 if __name__ == "__main__":
-    main()
+    # CONFIGPATH = "s3://~~~/config.json"
+    CONFIGPATH = "../test/config.json" ## here, you need to put your config file path
+    # open json file to import the api key
+    with open(CONFIGPATH) as f:
+        cfg = json.load(f)
+    # set OpenAI API KEY
+    os.environ['OPENAI_API_KEY'] = cfg['API_KEY']['OPENAI']
+    main(cfg)
