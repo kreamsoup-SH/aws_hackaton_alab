b009716b2c2efc7b25449ff493e8f4a21c2c46ad
kreamsoup-SH <alexalex225225@gmail.com>
2024-02-17 11:16:38 +0900

    updated

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -5,26 +5,37 @@ from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders.csv_loader import CSVLoader
 
+# CONFIGPATH = "s3://~~~/config.json"
+CONFIGPATH = "../config.json" ## here, you need to put your config file path
+
+# open json file to import the api key
+with open(CONFIGPATH) as f:
+    cfg = json.load(f)
+
+# set OpenAI API KEY
+os.environ['OPENAI_API_KEY'] = cfg['API_KEY']['OPENAI']
+
+
 def lambda_handler(event, context):
-    s3 = boto3.client('s3')
-    loadpath='s3://alab-s3/data/gov_portal.csv'
-    filename="faiss_index_ALAB"
+    s3 = boto3.client('s3') # should we put this in the function? or put it outside of the function?
+    
+    csvloadpath='s3://alab-s3/data/gov_portal.csv'
+    outfilename="faiss_index_ALAB"
 
     # index 생성
-    make_index(loadpath, filename)
+    faiss_index = make_index(csvloadpath, outfilename)
     
     # s3에 업로드
-    s3.upload_file(f'./{filename}.faiss', 'alab-s3', f'{filename}.faiss')
-    s3.upload_file(f'./{filename}.pkl', 'alab-s3', f'{filename}.pkl')
+    # s3.upload_file(f'./{outfilename}.faiss', 'alab-s3', f'{outfilename}.faiss')
+    # s3.upload_file(f'./{outfilename}.pkl', 'alab-s3', f'{outfilename}.pkl')
+    faiss_index.save_local("s3://alab-s3/db/",outfilename)
 
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
 
-def make_index(csvloadpath,outfilename):
-    # OpenAI API 키 설정
-    os.environ['OPENAI_API_KEY'] = "<OPENAI_API_KEY_HERE>"
+def make_index(csvloadpath):
     embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
     # FAISS 인덱스 로드 또는 생성
     print('CSV에서 embedding 생성')
@@ -39,6 +50,6 @@ def make_index(csvloadpath,outfilename):
     pages = loader.load()
     # print(pages)
     faiss_index = FAISS.from_documents(pages, embeddings)
-    faiss_index.save_local("./",outfilename)
+    return faiss_index
 
 
